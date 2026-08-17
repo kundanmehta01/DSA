77f51c0c7dddfb2b56e8ece9cbc5e9c0ae9ff820
kundanmehta01 <kundanmehta8210@gmail.com>
2026-08-17 20:15:57 +0530

    added length check in Valid Anagram

--- HashMap/Valid Anagram.py
+++ HashMap/Valid Anagram.py
@@ -1,22 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        freq1={}
-        freq2={}
+        if len(s) != len(t):
+            return False
+
+        freq1 = {}
+        freq2 = {}
 
         for i in s:
             if i in freq1:
-                freq1[i]=freq1[i]+1
-
-            freq1[i]=1
+                freq1[i] = freq1[i] + 1
+            else:
+                freq1[i] = 1
 
         for j in t:
             if j in freq2:
-                freq2[j]=freq2[j]+1
+                freq2[j] = freq2[j] + 1
+            else:
+                freq2[j] = 1
 
-            freq2[j]=1
-
-        if freq1 == freq2:
-             return True
-        else:
-            return False
-        
+        return freq1 == freq2

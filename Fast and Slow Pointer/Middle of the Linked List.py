96aeafd67c5ff73a354cf44de16b24972b369167
kundanmehta01 <kundanmehta8210@gmail.com>
2026-08-31 18:56:33 +0530

    876. Middle of the Linked List (Fast & Slow Pointer)

---
+++ Fast and Slow Pointer/Middle of the Linked List.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/middle-of-the-linked-list/description
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+
+        while fast!=None and fast.next !=None:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow

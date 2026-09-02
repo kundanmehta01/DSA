d8ac52ddaed51b8a0aefc090d2376e98663ec794
kundanmehta01 <kundanmehta8210@gmail.com>
2026-09-02 08:59:57 +0530

    83. Remove Duplicates from Sorted List

--- Two_Pointers/5. Remove Duplicates from Sorted Array.py
+++ Two_Pointers/5. Remove Duplicates from Sorted Array.py
@@ -3,9 +3,8 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         k=1
-        l=0
 
-        for i in range (len(nums)):
+        for i in range (1,len(nums)):
             if nums[i]!=nums[k-1]:
                 nums[i],nums[k]=nums[k],nums[i]
                 k+=1

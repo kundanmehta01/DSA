06315d363cdbe414d44eb6e90fb7e8dc4f3090bc
kundanmehta01 <kundanmehta8210@gmail.com>
2026-09-13 06:30:55 +0530

    33. Search in Rotated Sorted Array

---
+++ BinarySearch/Search in Rotated Sorted Array.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n=len(nums)
+        for i in range(n):
+            if nums[i] == target:
+                return i
+        return -1

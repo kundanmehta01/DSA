5c61cfba1b292ebb7aec01abdc581363447f9056
kundanmehta01 <kundanmehta8210@gmail.com>
2026-09-24 15:31:23 +0530

    239. Sliding Window Maximum(Using Queue

--- Sliding window/9.Sliding Window Maximum.py
+++ Sliding window/9.Sliding Window Maximum.py
@@ -20,5 +20,27 @@
 #         return res
 
 
+from collections import deque
 
+class Solution:
+    def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
+        left = 0
+        dq = deque()
+        res = []
+
+        for right in range(len(nums)):
+
+            while dq and nums[dq[-1]] <= nums[right]:
+                dq.pop()
+
+            dq.append(right)
+
+            if dq[0] < left:
+                dq.popleft()
+
+            if right - left + 1 == k:
+                res.append(nums[dq[0]])
+                left += 1
+
+        return res
         
